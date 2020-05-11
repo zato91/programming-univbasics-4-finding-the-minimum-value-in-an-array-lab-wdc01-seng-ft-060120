c000b527384341eb930301026b814c43411d75e6
zara ahmadi <zatokey@gmail.com>
2020-05-11 00:00:53 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,16 +1,19 @@
 require 'pry' 
 
 def find_min_value(array)
+ 
   count = 0 
-  min_val = 0
+  
   
   while count < array.length do 
-    if min_val >= array[count]
-      array[count] = min_val
+     min_val = -1
+    if min_val > array[count]
+       min_val = array[count]
       
     end
     count += 1 
-    binding.pry
+   
   end
   min_val
 end
+
